f34cbf1d9b8e6a225fd3b4996873aad1091404c8
RCX777 <robertcnst02@gmail.com>
2023-09-24 22:57:07 +0300

    Fixes typo in previous commit (SSH-ASKPASS -> SSH_ASKPASS)
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -29,7 +29,7 @@
       PROMPT="%F{121}%n%f@%F{121}%m%f:%F{147}%~/ %(?.%F{121}.%F{red})%#%f "
 
       # Disables annoying x11-ssh-askpass (login is now done in the terminal)
-      SSH-ASKPASS=""
+      SSH_ASKPASS=""
 
       # Binds Ctrl-Arrow(right/left)
       bindkey "^[[1;5C" forward-word

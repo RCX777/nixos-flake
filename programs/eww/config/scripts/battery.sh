6c7b3d668d25fae5a26a3d53c48d6a6596e5567e
RCX777 <robertcnst02@gmail.com>
2023-09-24 11:49:07 +0300

    Redesigns the battery widget, styles the right side of the top bar
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- programs/eww/config/scripts/battery.sh
+++ programs/eww/config/scripts/battery.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-case $1 in
-    -b|--battery-level)
-        LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
+# Get the battery level and charging status
+LEVEL=$(cat /sys/class/power_supply/BAT0/capacity)
+PLUGG=$(cat /sys/class/power_supply/AC0/online)
 
-        eww update battery_level=$LEVEL
+# Used to set the color of the battery icon
+if   [ $LEVEL -lt 25 ]; then
+    CLASS="battery_0_25"
+elif [ $LEVEL -lt 50 ]; then
+    CLASS="battery_25_50"
+elif [ $LEVEL -lt 75 ]; then
+    CLASS="battery_50_75"
+else
+    CLASS="battery_75_100"
+fi
 
+eww update battery_icon_style=$CLASS
+
+# Print the battery icon, according to level and charging status
+case $PLUGG in
+    0)
         case $LEVEL in
              [0-9]) printf "󰂎";;
             1[0-9]) printf "󰁺";;
@@ -20,7 +34,19 @@ case $1 in
             9[6-9]|100) printf "󰁹";;
         esac
         ;;
-    -p|--plugged-in)
-        cat /sys/class/power_supply/AC0/online
+    1)
+        case $LEVEL in
+             [0-9]) printf "󰢜";;
+            1[0-9]) printf "󰢜";;
+            2[0-9]) printf "󰂆";;
+            3[0-9]) printf "󰂇";;
+            4[0-9]) printf "󰂈";;
+            5[0-9]) printf "󰢝";;
+            6[0-9]) printf "󰂉";;
+            7[0-9]) printf "󰢞";;
+            8[0-9]) printf "󰂊";;
+            9[0-5]) printf "󰂋";;
+            9[6-9]|100) printf "󰂅";;
+        esac
         ;;
 esac

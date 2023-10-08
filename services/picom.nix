e375d1790b6f32403c201e1c485f40add770fb13
RCX777 <robertcnst02@gmail.com>
2023-10-08 12:46:56 +0300

    improves fullscreen performance for games
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- services/picom.nix
+++ services/picom.nix
@@ -4,7 +4,12 @@
 
     settings = {
       # General
-      vsync = true;
+      vsync = false;
+
+      use-ewmh-active-win = true; # ewmh is active in xmonad.hs
+      no-ewmh-fullscreen  = true;
+
+      unredir-if-possible = true;
 
       # Blur
       blur = {
@@ -36,6 +41,14 @@
         desktop = { shadow = true; };
         tooltip = { opacity = 0.8; };
       };
+
+      # Excluding games from compositing
+      rounded-corners-exclude = [
+        "class_g = 'cs2'"
+      ];
+      blur-background-exclude = [
+        "class_g = 'cs2'"
+      ];
     };
 
     backend = "glx";

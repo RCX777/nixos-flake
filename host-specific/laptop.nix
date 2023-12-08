8c64358eb93a4b73850c6947f616570ed23cf657
RCX777 <robertcnst02@gmail.com>
2023-12-08 17:21:13 +0200

    fixes previous commit, by resolving double declaration
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- host-specific/laptop.nix
+++ host-specific/laptop.nix
@@ -2,8 +2,8 @@
   
   # TODO: for some reason lightdm is not working properly on pc
   #       using sddm is a workaround! fix in the future!
-  services.xserver.displayManager.sddm.enable = false;
-  services.xserver.displayManager.lightdm.greeters.mini.enable = true;
+  services.xserver.displayManager.sddm.enable = lib.mkForce false;
+  services.xserver.displayManager.lightdm.greeters.mini.enable = lib.mkForce true;
 
   # TODO: re-add battery widget to bar.yuck
 }

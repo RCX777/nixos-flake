c8e6b7d1f1a11234a888aa09c3705adb02580c5a
RCX777 <robertcnst02@gmail.com>
2024-03-23 22:32:38 +0200

    adds `nixd` to the list of LSPs for `neovim`
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- programs/neovim/config/lua/configs/lspconfig.lua
+++ programs/neovim/config/lua/configs/lspconfig.lua
@@ -4,7 +4,11 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "clangd", "pylsp" }
+local servers = {
+  "clangd",
+  "pylsp",
+  "nixd"
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do

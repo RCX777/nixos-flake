6ff75efcb5d78b452ed3d885ff3b631e37d41b7f
RCX777 <robertcnst02@gmail.com>
2024-03-24 23:32:56 +0200

    Adds keymaps for moving selection up/down and sets global indent to 4 spaces
    Signed-off-by: RCX777 <robertcnst02@gmail.com>

--- programs/neovim/config/lua/mappings.lua
+++ programs/neovim/config/lua/mappings.lua
@@ -1,10 +1,16 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
+
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+
+-- move selected lines up/down
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<M-Up>",   ":m '<-2<CR>gv=gv")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
--- programs/neovim/config/lua/options.lua
+++ programs/neovim/config/lua/options.lua
@@ -9,3 +9,7 @@ local wo = vim.wo
 wo.number = true
 wo.relativenumber = true
 
+o.tabstop = 4      -- A TAB character looks like 4 spaces
+o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
+o.shiftwidth = 4   -- Number of spaces inserted when indenting
